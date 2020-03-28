f1dd494e4fdd4ca58251df0153b99105ffc95e1a
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-03-28 11:50:11 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,27 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj =new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(200);
+        obj.insert(30);
+        obj.insert(50);
+        obj.insert(1);
+
+        obj.insert(4);
+
+        obj.insert(88);
+        System.out.println("post order traversl");
+                obj.postorder(obj.root);
+
+        System.out.println();
+        System.out.println("left values traversl");
+
+        obj.printleft(obj.root);
+        System.out.println("nodes not having left nodes");
+
+        System.out.println( obj.getCount());
+
+    }
 }
