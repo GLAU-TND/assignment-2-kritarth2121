03a6bdb1ae1ae6a316340ffbe7da8db278dea833
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-03-28 11:52:51 +0530

    Update TreeNode.java

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,41 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+    TreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    public TreeNode() {
+
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
 }
+
