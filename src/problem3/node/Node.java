d46a7ac40107d1140de4f9bf19b5071d0edf1c57
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-03-28 13:02:04 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,35 @@
 package problem3.node;
 
 public class Node {
+    private Student student;
+    private Node next;
+    Node(Student student){
+        this.student=student;
+        this.next=null;
+    }
+    public Node(){
+
+    }
+
+    public Node getNode() {
+        return next;
+    }
+
+    public void setNode(Node node) {
+        this.next = node;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
+
