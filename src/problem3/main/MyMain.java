e2fd81be779f2950e7a329833a9c7704c79fe2b0
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-03-28 13:00:57 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,16 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+MyPriorityQueue obj=new MyPriorityQueue();
+obj.insert(33,"Kriarth");
+        obj.insert(32,"Prashant");
+        obj.insert(31,"Akanksha");
+        obj.insert(30,"Priyanka");
+        obj.insert(21,"Shashwat");
+        obj.print();
+
+
+
+    }
 }
